6a18c9a1b911489490b755a2712aeedee898b197
blankek <kombarus05@gmail.com>
2025-02-11 10:18:12 +0500

    Add Boostrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,17 @@
 package com.example.demo.model;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text, Integer likes){
+    public Post(String text, Date creationDate, Integer likes){
         this.text=text;
-        this.likes=likes;
+        this.creationDate = creationDate;
+        this.likes = likes;
     }
 
     public Integer getLikes() {
@@ -15,5 +20,9 @@ public class Post {
 
     public String getText() {
         return text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,14 +4,15 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
-        Post first = new Post("Первый элемент",15);
-        Post second = new Post("Второй элемент", 10);
-        Post third = new Post("Третий элемент", 12);
+        Post first = new Post("Первый элемент",new Date(), 15);
+        Post second = new Post("Второй элемент",new Date(), 10);
+        Post third = new Post("Третий элемент",new Date(), 12);
 
         List<Post> list = new ArrayList<>();
         list.add(first);
